2bd57eb5818fe3310d35937b59951bc65d16baf1
skell94 <svenkell94@gmail.com>
2018-07-18 20:37:39 +0200

    add switch_statement to corruption

--- corruptors/java_corruptor.py
+++ corruptors/java_corruptor.py
@@ -8,13 +8,14 @@ import re
 BRACKETS = ['(', ')', '[', ']', '{', '}']
 
 def corrupt(s):
-    s = s.strip()
-    if random.random() > 0.9 and len(s) > 1:
+    if random.random() > 0.9:
         s = _remove_bracket(s)
-    if random.random() > 0.9 and len(s) > 1:
+    if random.random() > 0.9:
         s = _remove_semicolon(s)
-    if random.random() > 0.9 and len(s) > 1:
-        s = _add_typo(s)
+    if random.random() > 0.9:
+        s = _misspell_variable(s)
+    if random.random() > 0.9:
+        s = _switch_statement_lines(s)
     return s
 
 def _remove_bracket(s):
@@ -31,24 +32,24 @@ def _remove_semicolon(s):
         s = s[:drop_index] + s[drop_index+1:]
     return s
 
-def _add_typo(s):
-    change_char = random.randint(0, len(s)-2)
-    s = s[:change_char] + s[change_char+1] + s[change_char] + s[change_char+2:]
-    return s
+# def _add_typo(s):
+#     change_char = random.randint(0, len(s)-2)
+#     s = s[:change_char] + s[change_char+1] + s[change_char] + s[change_char+2:]
+#     return s
 
 def _misspell_variable(s):
     tree = javalang.parse.parse(s)
     variables = []
     for _, node in tree.filter(javalang.tree.VariableDeclarator):
         variables.append(node)
-        
+
     var_name = random.choice(variables).name
     occurances = []
     for occurance in re.finditer(r'\b(' + var_name + r')\b', s):
         occurances.append(occurance)
 
     if len(occurances) <= 1:
-        return
+        return s
 
     chosen_occurance = random.choice(occurances[1:])
     r = random.random()
@@ -64,3 +65,39 @@ def _misspell_variable(s):
         s = s[:chosen_occurance.start()] + var_name[:change_char] + var_name[change_char+1] + var_name[change_char] + var_name[change_char+2:] + s[chosen_occurance.end():]
 
     return s
+
+def _switch_statement_lines(s):
+    tree = javalang.parse.parse(s)
+    statements = []
+    for _, node in tree.filter(javalang.tree.LocalVariableDeclaration):
+        start = node.position[1]
+        end = s[start:].find(';') + start + 1
+        statements.append({'class': type(node), 'start': start, 'end': end})
+
+    for _, node in tree.filter(javalang.tree.StatementExpression):
+        node = node.children[1]
+        if not isinstance(node, javalang.tree.Assignment) and not isinstance(node, javalang.tree.MethodInvocation):
+            continue
+        if isinstance(node, javalang.tree.Assignment):
+            start = node.children[0].position[1]
+        else:
+            start = node.position[1]
+        end = s[start:].find(';') + start + 1
+        statements.append({'class': type(node), 'start': start, 'end': end})
+
+    if len(statements) <= 1:
+        return s
+
+    statements = sorted(statements, key=lambda x: x['start'])
+    possible_statements = []
+    previous_statement = statements[0]
+    for statement in statements[1:]:
+        if previous_statement['end'] + 1 == statement['start'] and previous_statement['class'] != statement['class']:
+            possible_statements.append((previous_statement, statement))
+        previous_statement = statement
+
+    if len(possible_statements) == 0:
+        return s
+
+    first, second = random.choice(possible_statements)
+    return s[:first['start']] + s[second['start']:second['end']] + s[first['start']:first['end']] + s[second['end']:]
