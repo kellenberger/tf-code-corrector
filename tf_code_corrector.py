c07fe61231788243f1ef2a33b51c3f54665021ec
skell94 <svenkell94@gmail.com>
2018-08-03 15:52:24 +0200

    rework iterator and some flags

--- tf_code_corrector.py
+++ tf_code_corrector.py
@@ -16,32 +16,36 @@ from corruptors import java_corruptor
 
 tf.app.flags.DEFINE_string("data_directory", "", "Directory of the data set")
 tf.app.flags.DEFINE_string("output_directory", "", "Output directory for checkpoints and tests")
-tf.app.flags.DEFINE_integer("max_sequence_length", 1000, "Max length of input sequence")
-tf.app.flags.DEFINE_integer("sequence_length_step", 250, "Step size in which the sequence length is increased")
-tf.app.flags.DEFINE_integer("sequence_length_increase_iterations", 5000, "Number of iterations after which the sequence_length is increased")
-tf.app.flags.DEFINE_integer("simple_corruption_threshold", 20000, "Number of total iterations after which the difficulty of the corruption is increased")
+tf.app.flags.DEFINE_integer("max_sequence_length", 300, "Max length of input sequence")
 tf.app.flags.DEFINE_integer("pad_id", 1, "Code of padding character")
 tf.app.flags.DEFINE_integer("sos_id", 2, "Code of start-of-sequence character")
 tf.app.flags.DEFINE_integer("eos_id", 3, "Code of end-of-sequence character")
-tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size for training input")
+tf.app.flags.DEFINE_integer("eol_id", 4, "Code of end-of-line-character")
+tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size for training input")
 tf.app.flags.DEFINE_integer("num_layers", 4, "Number of layers of the network")
 tf.app.flags.DEFINE_integer("num_units", 256, "Number of units in each layer")
-tf.app.flags.DEFINE_integer("num_iterations", 40000, "Number of iterations in training")
+tf.app.flags.DEFINE_integer("num_iterations", 20000, "Number of iterations in training")
 tf.app.flags.DEFINE_integer("eval_steps", 1000, "Step size for evaluation")
 tf.app.flags.DEFINE_integer("random_seed", 1234, "Seed for all random operations")
 tf.app.flags.DEFINE_integer("num_gpus", 1, "number of GPUs to use")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm")
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate for the optimizer")
+tf.app.flags.DEFINE_boolean("corrupt_all", True, "Wheter to corrupt all data or just some percentage")
 
 FLAGS = tf.app.flags.FLAGS
 
 def main(_):
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S/")
-    FLAGS.output_directory = os.path.join(FLAGS.output_directory, timestamp)
+
+    if os.path.isdir(FLAGS.output_directory):
+        raise ValueError("output directory must not exist yet")
+
     os.makedirs(FLAGS.output_directory)
 
     with open(os.path.join(FLAGS.output_directory, 'hparams.json'), 'w') as hparam:
         json.dump(FLAGS.flag_values_dict(), hparam)
+
+    java_corruptor.EOL_ID = FLAGS.eol_id
+    java_corruptor.CORRUPT_ALL = FLAGS.corrupt_all
 
     random.seed(FLAGS.random_seed)
 
@@ -63,9 +67,7 @@ def main(_):
     eval_sess = tf.Session(graph=eval_graph)
 
     train_sess.run(initializer)
-    initialize_iterator(train_iterator, 'train', FLAGS.sequence_length_step, train_placeholders, train_sess, simple=True)
-    print("Max sequence length set to {}".format(FLAGS.sequence_length_step))
-    sys.stdout.flush()
+    initialize_iterator(train_iterator, 'train', FLAGS.max_sequence_length, train_placeholders, train_sess)
     initialize_iterator(eval_iterator, 'eval', FLAGS.max_sequence_length, eval_placeholders, eval_sess)
 
     for i in range(FLAGS.num_iterations):
@@ -75,12 +77,7 @@ def main(_):
                 train_model.train(train_sess, i+1)
                 trained = True
             except tf.errors.OutOfRangeError:
-                step = (i+1) // FLAGS.sequence_length_increase_iterations
-                step += 1
-                current_sequence_length = step * FLAGS.sequence_length_step
-                if current_sequence_length > FLAGS.max_sequence_length:
-                    current_sequence_length = FLAGS.max_sequence_length
-                initialize_iterator(train_iterator, 'train', current_sequence_length, train_placeholders, train_sess, simple=True if i < FLAGS.simple_corruption_threshold else False)
+                initialize_iterator(train_iterator, 'train', FLAGS.max_sequence_length, train_placeholders, train_sess)
 
 
         if (i+1) % FLAGS.eval_steps == 0:
@@ -93,15 +90,6 @@ def main(_):
                     evaluated = True
                 except tf.errors.OutOfRangeError:
                     initialize_iterator(eval_iterator, 'eval', FLAGS.max_sequence_length, eval_placeholders, eval_sess)
-
-        if (i+1) % FLAGS.sequence_length_increase_iterations == 0:
-            step = (i+1) // FLAGS.sequence_length_increase_iterations
-            step += 1
-            new_sequence_length = step * FLAGS.sequence_length_step
-            if new_sequence_length <= FLAGS.max_sequence_length:
-                initialize_iterator(train_iterator, 'train', new_sequence_length, train_placeholders, train_sess, simple=True if i < FLAGS.simple_corruption_threshold else False)
-                print("Max sequence length increased to {}".format(new_sequence_length))
-                sys.stdout.flush()
 
 
 def initialize_iterator(iterator, file_name, sequence_length, placeholders, sess, simple=False):
