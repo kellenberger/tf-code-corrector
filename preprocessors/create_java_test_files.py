c04752af79828be8611f18bf8f3e40ee557bd46c
skell94 <svenkell94@gmail.com>
2018-08-03 15:52:42 +0200

    enhance creation of test files

--- preprocessors/create_java_test_files.py
+++ preprocessors/create_java_test_files.py
@@ -13,10 +13,7 @@ tf.app.flags.DEFINE_integer("lines_per_file", 8192, "Lines in each test file")
 
 FLAGS = tf.app.flags.FLAGS
 
-def do_nothing(line):
-    return line
-
-def create_test_file(dir, file_name, project_files, pertubation_fn, unchanged_allowed = False):
+def create_uncorrupted_file(dir, file_name, project_files):
     line_count = 0
     with open(os.path.join(dir, file_name + '.src'), 'w') as source_file, \
             open(os.path.join(dir, file_name + '.tgt'), 'w') as target_file:
@@ -28,19 +25,34 @@ def create_test_file(dir, file_name, project_files, pertubation_fn, unchanged_al
                 break
             with open(project, 'r') as project_data:
                 lines = project_data.readlines()
-                lines = [line for line in lines if len(line) <= FLAGS.max_sequence_length]
-                if len(lines) < 500:
-                    continue
-                random_lines = random.sample(lines, 500)
-                for line in random_lines:
+                suitable_lines = [line for line in lines if len(line) <= FLAGS.max_sequence_length]
+                random.shuffle(suitable_lines)
+                for line in suitable_lines:
                     if line_count == FLAGS.lines_per_file:
                         break
                     tgt_line = line.strip()
-                    src_line = pertubation_fn(line.strip())
-                    if src_line and (src_line != tgt_line or unchanged_allowed):
+                    src_line = tgt_line
+                    if src_line:
                         source_file.write(src_line + "\n")
                         target_file.write(tgt_line + "\n")
                         line_count += 1
+
+def create_corrupted_file(dir, file_name, uncorrupted_file, corruption_fn):
+    with open(os.path.join(dir, uncorrupted_file + '.tgt'), 'r') as uncorrupted_data, \
+            open(os.path.join(dir, file_name + '.src'), 'w') as source_file, \
+            open(os.path.join(dir, file_name + '.tgt'), 'w') as target_file:
+        for line in uncorrupted_data:
+            tgt_line = line.strip()
+
+            class_start = "public class A{"
+            class_end = "}"
+            s = class_start + tgt_line + class_end
+            s = corruption_fn(s)
+            src_line = s[len(class_start):len(s) - len(class_end)]
+            if tgt_line == src_line:
+                print 'unable to corrupt'
+            source_file.write(src_line + "\n")
+            target_file.write(tgt_line + "\n")
 
 
 def main(_):
@@ -50,18 +62,19 @@ def main(_):
 
     test_projects = glob.glob(os.path.join(FLAGS.java_directory, 'test*.java'))
 
+    print('Create Uncorrupted')
+    create_uncorrupted_file(test_directory, 'uncorrupted', test_projects)
     print('Create Brackets')
-    create_test_file(test_directory, 'brackets', test_projects, java_corruptor._remove_bracket)
+    create_corrupted_file(test_directory, 'brackets', 'uncorrupted', java_corruptor._remove_bracket)
     print('Create Semicolon')
-    create_test_file(test_directory, 'semicolon', test_projects, java_corruptor._remove_semicolon)
+    create_corrupted_file(test_directory, 'semicolon', 'uncorrupted', java_corruptor._remove_semicolon)
     print('Create Variable')
-    create_test_file(test_directory, 'variable', test_projects, java_corruptor._misspell_variable)
+    create_corrupted_file(test_directory, 'variable', 'uncorrupted', java_corruptor._misspell_variable)
     print('Create Switch')
-    create_test_file(test_directory, 'switch', test_projects, java_corruptor._switch_statement_lines)
+    create_corrupted_file(test_directory, 'switch', 'uncorrupted', java_corruptor._switch_statement_lines)
     print ('Method return')
-    create_test_file(test_directory, 'method_return', test_projects, java_corruptor._change_method_return)
-    print('Create Unperturbed')
-    create_test_file(test_directory, 'unperturbed', test_projects, do_nothing, unchanged_allowed=True)
+    create_corrupted_file(test_directory, 'method_return', 'uncorrupted', java_corruptor._change_method_return)
+
 
 if __name__ == "__main__":
     tf.app.run()
